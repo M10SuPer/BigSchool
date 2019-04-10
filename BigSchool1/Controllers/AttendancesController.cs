2f143fef1839fbfaf5249e017fac32eb8b6b1e9f
NhanCao <nhanak47@gmail.com>
2019-04-10 20:19:23 +0700

    20:19 10/4

--- BigSchool1/Controllers/AttendancesController.cs
+++ BigSchool1/Controllers/AttendancesController.cs
@@ -1,4 +1,5 @@
-﻿using BigSchool1.Models;
+﻿using BigSchool1.DTOs;
+using BigSchool1.Models;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
@@ -21,14 +22,14 @@ namespace BigSchool1.Controllers
         }
 
         [HttpPost]
-        public IHttpActionResult Attend([FromBody] int courseId)
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
         {
             var userId = User.Identity.GetUserId();
-            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == courseId))
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
                 return BadRequest("The Attendance already exists!");
             var attendance = new Attendance
             {
-                CourseId = courseId,
+                CourseId = attendanceDto.CourseId,
                 AttendeeId = userId
             };
             _dbContext.Attendances.Add(attendance);
